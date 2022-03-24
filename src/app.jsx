b86965b8110a066a7804173102f4cfae16f2e54e
confidential-nt <youyoy689@gmail.com>
2022-03-24 14:27:45 +0900

    habits

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import "./app.css";
+
+import React, { Component } from "react";
+import Habits from "./components/habits";
+
+class App extends Component {
+  render() {
+    return <Habits />;
+  }
+}
+
+export default App;
---
+++ src/components/habits.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Habit from "./habit";
+
+class Habits extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Running", count: 0 },
+      { id: 2, name: "Coding", count: 0 },
+      { id: 3, name: "Studying", count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    if (habits[index].count > 0) {
+      habits[index].count--;
+      this.setState({ habits });
+    }
+  };
+
+  handleDelete = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits.splice(index, 1);
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <ul>
+        {this.state.habits.map((habit) => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default Habits;
