7414fbe174ee01edd4490f5c87bb28a44c61431f
confidential-nt <youyoy689@gmail.com>
2022-03-29 22:14:19 +0900

    add form

---
+++ src/components/habitAddForm.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class HabitAddForm extends Component {
+  render() {
+    return (
+      <form className="add-form">
+        <input type="text" className="add-input" placeholder="Habit" />
+        <button type="submit" className="add-button">
+          Add
+        </button>
+      </form>
+    );
+  }
+}
+
+export default HabitAddForm;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,51 +1,36 @@
 import React, { Component } from "react";
 import Habit from "./habit";
+import HabitAddForm from "./habitAddForm";
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: "Running", count: 0 },
-      { id: 2, name: "Coding", count: 0 },
-      { id: 3, name: "Studying", count: 0 },
-    ],
-  };
-
   handleIncrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-    this.setState({ habits });
+    this.props.onIncrement(habit);
   };
 
   handleDecrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    if (habits[index].count > 0) {
-      habits[index].count--;
-      this.setState({ habits });
-    }
+    this.props.onDecrement(habit);
   };
 
   handleDelete = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits.splice(index, 1);
-    this.setState({ habits });
+    this.props.onDelete(habit);
   };
 
   render() {
     return (
-      <ul>
-        {this.state.habits.map((habit) => (
-          <Habit
-            key={habit.id}
-            habit={habit}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onDelete={this.handleDelete}
-          />
-        ))}
-      </ul>
+      <>
+        <HabitAddForm />
+        <ul>
+          {this.props.habits.map((habit) => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.handleIncrement}
+              onDecrement={this.handleDecrement}
+              onDelete={this.handleDelete}
+            />
+          ))}
+        </ul>
+      </>
     );
   }
 }
